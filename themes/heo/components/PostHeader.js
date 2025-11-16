52b447182cd82b4b5bdb46ff7987ff0642db2f3f
mecloo <112839914+mecloo@users.noreply.github.com>
2025-11-16 17:05:12 +0800

    Update PostHeader.js

--- themes/heo/components/PostHeader.js
+++ themes/heo/components/PostHeader.js
@@ -21,7 +21,7 @@ export default function PostHeader({ post, siteInfo, isDarkMode }) {
   const ANALYTICS_BUSUANZI_ENABLE = siteConfig('ANALYTICS_BUSUANZI_ENABLE')
   return (
     <div
-      id='post-bg'
+    //  id='post-bg'
    // className='md:mb-0 -mb-5 w-full h-[30rem] relative md:flex-shrink-0 overflow-hidden bg-cover bg-center bg-no-repeat z-10'>
     {/*
       <style jsx>{`
