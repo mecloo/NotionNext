26053f7755f3301aead6ad5775a9dd3cc470b074
mecloo <112839914+mecloo@users.noreply.github.com>
2025-11-17 14:50:37 +0800

    Update PostHeader.js

--- themes/heo/components/PostHeader.js
+++ themes/heo/components/PostHeader.js
@@ -46,7 +46,7 @@ export default function PostHeader({ post, siteInfo, isDarkMode }) {
           className='coverdiv lg:opacity-50 lg:translate-x-96 lg:rotate-12'>
           <LazyImage
             id='post-cover'
-            className='w-full h-full object-cover max-h-[50rem] min-w-[50vw] min-h-[20rem]'
+            className='w-full h-full object-cover max-h-[50rem] min-w-[50vw] min-h-[15rem]'
             src={headerImage}
           />
         </div>
